309c455e88a614ee7498bd9f4146b4c281ac0d6c
wallansag <wallans.gomes@fatec.sp.gov.br>
2024-12-05 07:37:54 -0300

    Update script.js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,1 +1,29 @@
-//alert("Cookies: roubarei seus dados !!!");
+alert("Cookies: Para melhorar a sua experiência de navegação, coletamos parte do seus dados  !!!")
+
+//Versao 2.0: Lista Suspensa Melhorada
+function validacaoteFormulario() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('telefone').value;
+    const gender = document.getElementById('genero').value;
+    const birthDate = document.getElementById('DataNascimento').value;
+
+    let errorMessage = '';
+
+    if (!name || !email || !phone || !gender || !birthDate) {
+        errorMessage = 'Por favor, preencha todos os campos.';
+    } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        errorMessage = 'Por favor, insira um e-mail válido.';
+    }
+
+    if (errorMessage) {
+        alert(errorMessage);
+    } else {
+        alert(`Dados inseridos:
+        Nome: ${name}
+        E-mail: ${email}
+        Telefone: ${phone}
+        Gênero: ${gender}
+        Data de Nascimento: ${birthDate}`);
+    }
+}
